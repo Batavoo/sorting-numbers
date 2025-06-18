57f92a7142e6b254a6fbecc773cc189167b84215
Gustavo Santos Arruda <gsarruda.office@gmail.com>
2025-06-18 15:41:14 -0300

    started the js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Buttons
+sortButton = document.querySelector(".submit-button .rainbow button");
+sortButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Sort button clicked");
+});
