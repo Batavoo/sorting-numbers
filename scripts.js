c4ddcd3297de4a203f6132420942292f1a930255
Gustavo Santos Arruda <gsarruda.office@gmail.com>
2025-06-18 16:58:13 -0300

    added inputs checks and button events

--- scripts.js
+++ scripts.js
@@ -1,6 +1,50 @@
+// Inputs
+const quantityInput = document.querySelector("#quantity");
+const minRangeInput = document.querySelector("#min");
+const maxRangeInput = document.querySelector("#max");
+const checkboxRepeatInput = document.querySelector("#allow-repeats");
+
+quantityInput.oninput = handleInputChange;
+minRangeInput.oninput = handleInputChange;
+maxRangeInput.oninput = handleInputChange;
+
+// Sorting Wrappers
+const sortWrapper = document.querySelector("#sort-form");
+const resultsWrapper = document.querySelector("#result-tab");
+
 // Buttons
-sortButton = document.querySelector(".submit-button .rainbow button");
+const sortButton = document.querySelector(".submit-button .rainbow button");
+const redoButton = document.querySelector(".redo-button .rainbow button");
+
 sortButton.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log("Sort button clicked");
+  let allInputsFilled =
+    !quantityInput.value || !minRangeInput.value || !maxRangeInput.value;
+  // Checar se tem mais coisas pra checar
+
+  if (allInputsFilled) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+  sortWrapper.classList.add("d-none");
+  resultsWrapper.classList.remove("d-none");
 });
+
+redoButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  quantityInput.value = "";
+  minRangeInput.value = "";
+  maxRangeInput.value = "";
+  checkboxRepeatInput.checked = false;
+  sortWrapper.classList.remove("d-none");
+  resultsWrapper.classList.add("d-none");
+});
+
+// Functions
+function handleInputChange(event) {
+  let value = event.target.value.replace(/\D/g, "");
+  if (value.length > 4) {
+    value = value.slice(0, 4);
+  }
+  event.target.value = value;
+}
